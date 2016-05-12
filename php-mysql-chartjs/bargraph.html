f5bc6179baeda70db0c05e6c21679a13c6d272fa
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2016-05-12 21:10:33 +0530

    php-mysql-chartjs

---
+++ php-mysql-chartjs/bargraph.html
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>ChartJS - BarGraph</title>
+		<style type="text/css">
+			#chart-container {
+				width: 640px;
+				height: auto;
+			}
+		</style>
+	</head>
+	<body>
+		<div id="chart-container">
+			<canvas id="mycanvas"></canvas>
+		</div>
+
+		<!-- javascript -->
+		<script type="text/javascript" src="js/jquery.min.js"></script>
+		<script type="text/javascript" src="js/Chart.min.js"></script>
+		<script type="text/javascript" src="js/app.js"></script>
+	</body>
+</html>
---
+++ php-mysql-chartjs/js/app.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+	$.ajax({
+		url: "http://localhost/chartjs/data.php",
+		method: "GET",
+		success: function(data) {
+			console.log(data);
+			var player = [];
+			var score = [];
+
+			for(var i in data) {
+				player.push("Player " + data[i].playerid);
+				score.push(data[i].score);
+			}
+
+			var chartdata = {
+				labels: player,
+				datasets : [
+					{
+						label: 'Player Score',
+						backgroundColor: 'rgba(200, 200, 200, 0.75)',
+						borderColor: 'rgba(200, 200, 200, 0.75)',
+						hoverBackgroundColor: 'rgba(200, 200, 200, 1)',
+						hoverBorderColor: 'rgba(200, 200, 200, 1)',
+						data: score
+					}
+				]
+			};
+
+			var ctx = $("#mycanvas");
+
+			var barGraph = new Chart(ctx, {
+				type: 'bar',
+				data: chartdata
+			});
+		},
+		error: function(data) {
+			console.log(data);
+		}
+	});
+});
